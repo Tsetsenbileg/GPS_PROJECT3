0f0441c48c044bb9afae9598c30adc3a532d5f8b
Tsetsenbileg <tsetsenbileg0413@gmail.com>
2022-03-29 18:35:44 +0800

    using dll

--- GPS_LIBRARY/GPS_LIBRARY/Packet.cs
+++ GPS_LIBRARY/GPS_LIBRARY/Packet.cs
@@ -5,7 +5,7 @@ namespace GPS_LIBRARY
 {
     public class Packet
     {
-        //Tsetsenbileg
+       
         public byte[] Header = new byte[2];
         public ushort Length = 0;
         public byte[] UNIT_code = new byte[12];
--- GPS_PROJECT3/Program.cs
+++ GPS_PROJECT3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GPS_LIBRARY;
+using System;
 using System.Linq;
 
 namespace GPS_PROJECT3
@@ -9,21 +10,11 @@ namespace GPS_PROJECT3
         {
 
             String dd = "404056003247512d31363031303633380110060a130a2a080f3e40440a7089e616cf005004d03101010505050006000101000018a5a6a7a8a9aaf2adaeafb0b16162636465666768696a6b6c060a130a2a0753e80d0a";
-            byte[] gg = StringToByteArray(dd);
-
+            byte[] gg = GPS_LIBRARY.Packet.StringToByteArray(dd);
             Packet p = new Packet(gg);
             Console.WriteLine(p.getIsRealValue());
             Console.WriteLine(p.getEventCode());
             Console.WriteLine(p.getPacketName());
-            //Console.WriteLine(p.byteToASCII(p.Event_Code));
-        }
-
-        public static byte[] StringToByteArray(string hex)
-        {
-            return Enumerable.Range(0, hex.Length)
-                             .Where(x => x % 2 == 0)
-                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                             .ToArray();
         }
     }
 }
